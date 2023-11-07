3bbea8bcb5ba421c390abe59f27d6130b5f46a39
vidya Joshi <148187787+vidyajoshi11@users.noreply.github.com>
2023-11-07 15:24:16 +0530

    Create simple.cpp

---
+++ simple.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=5,b=10,temp;
+cout<<"Before swapping:-"<<"/n";
+cout<<"a="<<a<<"b="<<b;
+temp=a;
+a=b;
+b=temp;
+cout<<"After swapping:-";
+cout<<"a="<<a<<"b="<<b;
+return 0;
+}
